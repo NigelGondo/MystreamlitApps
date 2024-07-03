435c365b70f007424c0e919b494c94a5247dc2bf
Nigel Gondo <86373350+NigelGondo@users.noreply.github.com>
2024-07-03 19:22:37 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st 
 import plotly.express as px 
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
 import altair as alt
 
